936e41b7fc0bb1b1d789e3d6802e216b9691e99a
Ryoga-exe <git@ryoga.dev>
2026-01-24 11:15:16 +0900

    feat: riscv-tests rv64

--- core/tools/riscv-tests/export_riscv_tests.sh
+++ core/tools/riscv-tests/export_riscv_tests.sh
@@ -8,19 +8,21 @@ SRC="/opt/riscv-tests-install/share/riscv-tests/isa"
 mkdir -p "$OUTDIR" "$OUTDIR/bin" "$OUTDIR/elf"
 
 shopt -s nullglob
-for f in "$SRC"/rv32ui-p-* "$SRC"/rv32mi-p-*; do
-  # .dump などは除外
+for f in \
+  "$SRC"/rv32ui-p-* "$SRC"/rv32mi-p-* \
+  "$SRC"/rv64ui-p-* "$SRC"/rv64mi-p-* \
+; do
   case "$f" in
     *.dump|*.hex|*.bin|*.elf|*.o|*.S|*.ld) continue ;;
   esac
 
-  if ! riscv32-unknown-elf-readelf -h "$f" >/dev/null 2>&1; then
+  if ! riscv64-unknown-elf-readelf -h "$f" >/dev/null 2>&1; then
     continue
   fi
 
   base="$(basename "$f")"
   cp -f "$f" "$OUTDIR/elf/${base}.elf"
-  riscv32-unknown-elf-objcopy -O binary "$f" "$OUTDIR/bin/${base}.bin"
+  riscv64-unknown-elf-objcopy -O binary "$f" "$OUTDIR/bin/${base}.bin"
   python3 /usr/local/bin/bin2hex.py "$BYTES_PER_LINE" "$OUTDIR/bin/${base}.bin" > "$OUTDIR/${base}.hex"
 done
 
