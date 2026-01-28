f15f0e09213c98cf7055ef14f4eb198f22227899
Ryoga-exe <git@ryoga.dev>
2026-01-28 20:52:35 +0900

    feat: update tools for rv64ua-p-* tests

--- core/tools/riscv-tests/export_riscv_tests.sh
+++ core/tools/riscv-tests/export_riscv_tests.sh
@@ -12,6 +12,7 @@ for f in \
   "$SRC"/rv32ui-p-* "$SRC"/rv32mi-p-* \
   "$SRC"/rv64ui-p-* "$SRC"/rv64mi-p-* \
   "$SRC"/rv64um-p-* "$SRC"/rv64um-p-* \
+  "$SRC"/rv64um-p-* "$SRC"/rv64ua-p-* \
 ; do
   case "$f" in
     *.dump|*.hex|*.bin|*.elf|*.o|*.S|*.ld) continue ;;
