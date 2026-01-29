22c626c427edd0bc8f6ed8bb9e61004ca1866092
Ryoga-exe <git@ryoga.dev>
2026-01-29 11:19:34 +0900

    refactor

--- core/test/zig/mswi.zig
+++ core/test/zig/mswi.zig
@@ -4,23 +4,10 @@ const msip0: *volatile u32 = @ptrFromInt(0x2000000);
 const mie_msie: u64 = 1 << 3;
 const mstatus_mie: u64 = 1 << 3;
 
-// CSR write helpers
-inline fn w_mtvec(x: u64) void {
-    asm volatile ("csrw mtvec, %[val]"
+inline fn csrw(comptime csr: []const u8, x: u64) void {
+    asm volatile ("csrw " ++ csr ++ ", %0"
         :
-        : [val] "r" (x),
-        : .{ .memory = true });
-}
-inline fn w_mie(x: u64) void {
-    asm volatile ("csrw mie, %[val]"
-        :
-        : [val] "r" (x),
-        : .{ .memory = true });
-}
-inline fn w_mstatus(x: u64) void {
-    asm volatile ("csrw mstatus, %[val]"
-        :
-        : [val] "r" (x),
+        : [x] "r" (x),
         : .{ .memory = true });
 }
 
@@ -29,9 +16,9 @@ export fn interrupt_handler() callconv(.{ .riscv64_interrupt = .{ .mode = .machi
 }
 
 pub export fn main() callconv(.c) noreturn {
-    w_mtvec(@intFromPtr(&interrupt_handler));
-    w_mie(mie_msie);
-    w_mstatus(mstatus_mie);
+    csrw("mtvec", @intFromPtr(interrupt_handler));
+    csrw("mie", mie_msie);
+    csrw("mstatus", mstatus_mie);
 
     msip0.* = 1;
 
